d5126eaed59519afc73705af2f4f22a03a188f0f
Vladislav <vlad.miller.06@bk.ru>
2024-04-13 10:56:49 +0700

    Добавьте файлы проекта.

---
+++ Пр Гид/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static _10._3.Program;
+
+namespace _10._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Что-то пишу. Потому что так нужно");
+        }
+        
+    }
+}
